b829dd628efdb3e2ccc52cbeb6ffcc4c77a83acd
so0p <charlsonsoo@gmail.com>
2019-10-30 11:32:30 -0700

    added src and test folders

---
+++ test/java_calculator/MainWindowTest.java
@@ -0,0 +1,14 @@
+package java_calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainWindowTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
